121efd80c69667658b65a1a2ae26c877f4139a4c
Gutu Galuppo <galuppodev@gmail.com>
2020-12-01 10:14:32 +0100

    created Prototypes to handle styles

--- src/controller/WhatsAppContyroller.js
+++ src/controller/WhatsAppContyroller.js
@@ -3,10 +3,12 @@ class WhatsAppContyroller {
 	constructor() {
 		console.log("WhatsAppContyroller OK")
 
+		this.elementsPrototype();
 		this.loadElements();
+
 	}
 
-	loadElements(){
+	loadElements() {
 		this.el = {};
 
 		document.querySelectorAll('[id]').forEach(element => {
@@ -14,6 +16,59 @@ class WhatsAppContyroller {
 			this.el[Format.getCamelCAse(element.id)] = element;
 
 		})
+	}
+
+	elementsPrototype() {
+
+		Element.prototype.hide = function () {
+			this.style.display = 'none';
+			return this; // returning "this" allow us to line up as many methods as we need as
+			// show().css().addClass() and so on...
+		}
+		// testing hide method in the browser
+		// app.el.app.hide()
+
+		Element.prototype.show = function () {
+			this.style.display = 'block';
+			return this;
+		}
+
+		Element.prototype.toggle = function () {
+			this.style.display = (this.style.display === 'none') ? 'block' : 'none';
+			return this;
+		}
+
+		Element.prototype.on = function (events, func) {
+			events.split(' ').forEach(event => {
+				this.addEventListener(event, func);
+			})
+			return this;
+		}
+
+		Element.prototype.css = function (styles) {
+			for (let name in styles) {
+				this.style[name] = styles[name];
+			}
+			return this;
+		}
+
+		Element.prototype.addClass = function (name) {
+			this.classList.add(name);
+			return this;
+		}
+		Element.prototype.removeClass = function (name) {
+			this.classList.remove(name);
+			return this;
+		}
+		Element.prototype.toggleClass = function (name) {
+			this.classList.toggle(name);
+			return this;
+		}
+		Element.prototype.hasClass = function (name) {
+			return this.classList.contains(name);
+		}
+
+
 	}
 
 }
