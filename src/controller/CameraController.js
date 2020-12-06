67cb5b4e88d65e6fe07e71be9e645d91176b2932
Gutu Galuppo <galuppodev@gmail.com>
2020-12-06 21:21:39 +0100

    fixed deprecated method createObjectURL()

--- src/controller/CameraController.js
+++ src/controller/CameraController.js
@@ -7,7 +7,7 @@ export class CameraController {
 		navigator.mediaDevices.getUserMedia({
 			video: true
 		}).then(stream => {
-			this._videoEl.src = URL.createObjectURL(stream);
+			this._videoEl.srcObject = stream;
 			this._videoEl.play();
 		}).catch(err => {
 			console.error(err)
